32c73b38406056e34d9cedacb0c8c33154c75f73
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-19 20:51:48 +0300

    Задание 47

---
+++ Task_47/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
