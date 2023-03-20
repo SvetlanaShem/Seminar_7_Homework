de145cb3dc24ed08fff4e89f010260f71be4bc3f
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-20 15:07:23 +0300

    Задание 52

---
+++ Task_52/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+GetAverage(rows, columns, array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverage(int m, int n, int[,] arr)
+{
+for(int j = 0; j<n; j++)
+{
+    double sum = 0;
+    for(int i = 0; i<m; i++)
+    {
+       
+       sum = sum + arr[i,j];
+    }
+    Console.Write($"{sum/m} ");
+}
+}
